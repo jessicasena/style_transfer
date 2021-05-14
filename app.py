374d3d53b07fded23456c33dd112b7265baa6801
Jessica Sena <jessicasenasouza@gmail.com>
2021-05-14 17:11:53 -0300

    app improvements

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, flash, request, redirect, jsonify
 from PIL import Image
 from io import BytesIO
 import base64
@@ -23,14 +23,29 @@ def to_base64(img):
 app = Flask(__name__)
 
 
-@app.route('/stylize', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def style_transfer():
-    base64_content = request.files['content'].read()
-    base64_style = request.files['style'].read()
-    content_img = from_base64(base64_content)
-    style_img = from_base64(base64_style)
+    if request.method == 'POST':
+        if 'content' and 'style' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        base64_content = request.files['content'].read()
+        base64_style = request.files['style'].read()
 
-    out_img = stylize(content_img, style_img)
-    out_base64 = to_base64(out_img)
+        content_img = from_base64(base64_content)
+        style_img = from_base64(base64_style)
 
-    return jsonify({'image': str(out_base64)})
+        out_img = stylize(content_img, style_img)
+        out_base64 = to_base64(out_img)
+
+        return jsonify({'image': str(out_base64)})
+    return '''
+        <!doctype html>
+        <title>Make your own painting</title>
+        <h1>Upload a content and a style image</h1>
+        <form method=post enctype=multipart/form-data>
+          <input type=file name=content>
+          <input type=file name=style>
+          <input type=submit value=Upload>
+        </form>
+        '''
